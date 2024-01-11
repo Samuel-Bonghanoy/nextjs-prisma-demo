96977284b1b63d823129dff3eb429d37b8a87435
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-01-11 13:09:39 +0800

    feat: create Task component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { TaskType } from "@/types/task";
+import { db } from "@/db";
 
 interface TaskProps {
   Task: TaskType;
 }
 
 export default function Task({ Task }: TaskProps) {
-  console.log(Task);
-
   return (
-    <div className="stats shadow bg-slate-500 text-black">
+    <div className="stats shadow bg-slate-500 text-black flex items-center px-3">
+      <button className="btn btn-primary">Primary</button>
       <div className="stat">
         <div className="stat-title text-black">{Task.title}</div>
         <div className="stat-desc text-black">{Task.content}</div>
